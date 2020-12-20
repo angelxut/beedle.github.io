18abb3bce3819c99c8d4b8623763c350ea6be21c
axiao33 <angelx@gatech.edu>
2020-12-20 14:26:33 -0600

    Updates

---
+++ precache-manifest.3ff9a3a9c5c34f8a99a0b0c00355ff81.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a6199717678cfa8313050a9c59a641a",
+    "url": "/beedle.github.io/index.html"
+  },
+  {
+    "revision": "ce5a8b58fd368149d91b",
+    "url": "/beedle.github.io/static/css/2.3a395901.chunk.css"
+  },
+  {
+    "revision": "323d1243e751fba6baa3",
+    "url": "/beedle.github.io/static/css/main.00770f9f.chunk.css"
+  },
+  {
+    "revision": "ce5a8b58fd368149d91b",
+    "url": "/beedle.github.io/static/js/2.b21d9b49.chunk.js"
+  },
+  {
+    "revision": "a8b7f9925879c67c458b78e4fa827b7f",
+    "url": "/beedle.github.io/static/js/2.b21d9b49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "323d1243e751fba6baa3",
+    "url": "/beedle.github.io/static/js/main.8decd8fa.chunk.js"
+  },
+  {
+    "revision": "8961b6e82d184febb1cd",
+    "url": "/beedle.github.io/static/js/runtime-main.14c90793.js"
+  },
+  {
+    "revision": "5727f869509c87f4d25c2225f0968573",
+    "url": "/beedle.github.io/static/media/chevron-left.5727f869.svg"
+  },
+  {
+    "revision": "9c59704b1de34c9fbb54c100b907ac85",
+    "url": "/beedle.github.io/static/media/chevron-right.9c59704b.svg"
+  },
+  {
+    "revision": "cbeab2705cd1a8c5d774eac8ed9c9619",
+    "url": "/beedle.github.io/static/media/dollar-sign.cbeab270.svg"
+  },
+  {
+    "revision": "1c251c8ff2222207650b4f24a2159cbb",
+    "url": "/beedle.github.io/static/media/dot-pattern.1c251c8f.svg"
+  },
+  {
+    "revision": "a1ad24916d979e088c5e8c80b073f92b",
+    "url": "/beedle.github.io/static/media/email-illustration.a1ad2491.svg"
+  },
+  {
+    "revision": "5e030c93ee64514b9fcdc887289ce8a9",
+    "url": "/beedle.github.io/static/media/facebook-icon.5e030c93.svg"
+  },
+  {
+    "revision": "f9776f55173cd40b2b3f3fed1c7443c4",
+    "url": "/beedle.github.io/static/media/logo.f9776f55.svg"
+  },
+  {
+    "revision": "b2f804bda31dee415fc2b1ff7ba9315f",
+    "url": "/beedle.github.io/static/media/map-pin.b2f804bd.svg"
+  },
+  {
+    "revision": "ca394ec8a4754ab9db293dcac626bdcb",
+    "url": "/beedle.github.io/static/media/menu.ca394ec8.svg"
+  },
+  {
+    "revision": "5d0ff9bc5818d6498df9d90437127f90",
+    "url": "/beedle.github.io/static/media/star-icon.5d0ff9bc.svg"
+  },
+  {
+    "revision": "ac8ea2135e1379656f02ca2eea26cc87",
+    "url": "/beedle.github.io/static/media/star.ac8ea213.svg"
+  },
+  {
+    "revision": "50a3868db49539d53742411308690eba",
+    "url": "/beedle.github.io/static/media/svg-decorator-blob-5.50a3868d.svg"
+  },
+  {
+    "revision": "c86fb21a2396dc9a8149429f1cd451cf",
+    "url": "/beedle.github.io/static/media/svg-decorator-blob-7.c86fb21a.svg"
+  },
+  {
+    "revision": "7189052ca672d113f057d9a4f3cb5efe",
+    "url": "/beedle.github.io/static/media/twitter-icon.7189052c.svg"
+  },
+  {
+    "revision": "b0200bf7cbbb26c32989e7a0e4d19eef",
+    "url": "/beedle.github.io/static/media/x.b0200bf7.svg"
+  },
+  {
+    "revision": "45e500e6c6c6577b063b28adc39fb422",
+    "url": "/beedle.github.io/static/media/youtube-icon.45e500e6.svg"
+  }
+]);
